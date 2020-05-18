6f6a3cd930cecf58394e77b6536e5ff2755fcf58
Bartlomiej Tyrpien <bartlomiejtyrpien@BartlomiejsMBP2.home>
2020-05-18 19:31:12 -0400

    i dumb

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://bartlomein:bartman23@cluster0-1j5ox.mongodb.net/postthing?retryWrites=true&w=majority",
+  SECRET_KEY: "this is a very secretive password boo yerrr",
+};
